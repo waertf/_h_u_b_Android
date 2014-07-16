d6ffcab35020a687d94169d98dba6eb7d1674f7b
alonso <alonso@wavegis.com.tw>
2014-07-16 18:05:48 +0800

    comment "handler.postDelayed(this, 1000);" and add two thread class:AcceptThread and ConnectedThread

--- src/com/example/yourapp/WavegisService.java
+++ src/com/example/yourapp/WavegisService.java
@@ -2,6 +2,8 @@ package com.example.yourapp;
 
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,10 +13,15 @@ import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.widget.Toast;
+import org.apache.http.util.EncodingUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Method;
+import java.util.UUID;
 
 /**
  * Created by wavegis on 2014/7/8.
@@ -50,6 +57,8 @@ public class WavegisService extends Service {
         }
         setDiscoverableTimeout(closeDiscoverableTimeout);
         SendBroadcast("BT Discoverable");
+        Thread mInsecureAcceptThread = new AcceptThread(mBluetoothAdapter);
+        mInsecureAcceptThread.start();
         closeDiscoverableHandler.postDelayed(CloseDiscoverableRunnable,closeDiscoverableTimeout);
         /*
         Intent discoverableIntent = new
@@ -79,7 +88,7 @@ public class WavegisService extends Service {
             //log目前時間
             //Log.i("time:", new Date().toString());
             SendBroadcast("任務:路況");
-            handler.postDelayed(this, 1000);
+            //handler.postDelayed(this, 1000);
         }
     };
     private Runnable CloseDiscoverableRunnable=new Runnable(){
@@ -134,4 +143,114 @@ public class WavegisService extends Service {
             e.printStackTrace();
         }
     }
+
+    private class AcceptThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+        private  final String NAME = "BluetoothWavegis";
+        private  final UUID MY_UUID =
+                UUID.fromString("dc16d180-0cbe-11e4-9191-0800200c9a66");
+        public AcceptThread(BluetoothAdapter mBluetoothAdapter) {
+            // Use a temporary object that is later assigned to mmServerSocket,
+            // because mmServerSocket is final
+            BluetoothServerSocket tmp = null;
+            try {
+                // MY_UUID is the app's UUID string, also used by the client code
+                tmp = mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(NAME, MY_UUID);
+                //tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord(NAME, MY_UUID);
+            } catch (IOException e) { }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+            // Keep listening until exception occurs or a socket is returned
+            while (true) {
+                try {
+                    if(mmServerSocket!=null)
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    break;
+                }
+                // If a connection was accepted
+                if (socket != null) {
+                    // Do work to manage the connection (in a separate thread)
+                    Thread mConnectedThread = new ConnectedThread(socket);
+                    mConnectedThread.start();
+                    //manageConnectedSocket(socket);
+                    try {
+                        mmServerSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+        }
+
+        /** Will cancel the listening socket, and cause the thread to finish */
+        public void cancel() {
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+                    // Send the obtained bytes to the UI activity
+                    //mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                            //.sendToTarget();
+                    SendBroadcast(new String(buffer,"UTF-8"));
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) { }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+
+
+    }
 }
+
+
