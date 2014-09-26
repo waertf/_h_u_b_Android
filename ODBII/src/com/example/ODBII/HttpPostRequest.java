84565e039366c496336dd274dbf2094d6b1275a8
alonso <alonso@wavegis.com.tw>
2014-09-26 12:20:53 +0800

    add httpPostRequest class implement by runnable class

---
+++ ODBII/src/com/example/ODBII/HttpPostRequest.java
@@ -0,0 +1,32 @@
+package com.example.ODBII;
+
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wavegisAAA on 9/26/2014.
+ */
+public class HttpPostRequest implements Runnable {
+    private String parameter;
+    private String URL;
+    public HttpPostRequest(String URL,String parameter)
+    {
+        this.URL=URL;
+        this.parameter=parameter;
+    }
+    @Override
+    public void run() {
+        HashMap<String, String> myMap = new HashMap<String, String>();
+        String s = "SALES:0,SALE_PRODUCTS:1,EXPENSES:2,EXPENSES_ITEMS:3";
+        String[] pairs = parameter.split(",");
+        for (int i=0;i<pairs.length;i++) {
+            String pair = pairs[i];
+            String[] keyValue = pair.split(":");
+            myMap.put(keyValue[0], keyValue[1]);
+        }
+        String result = JieHttpClient.POST(URL,myMap);
+        Log.d(this.toString(),"send http post-" + parameter + "-result:" + result);
+    }
+}
--- ODBII/src/com/example/ODBII/MyActivity.java
+++ ODBII/src/com/example/ODBII/MyActivity.java
@@ -2,6 +2,7 @@ package com.example.ODBII;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.HashMap;
 
@@ -9,11 +10,12 @@ public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
-    private String _httpRequestUrl="";
+    private String _httpRequestUrl="http://192.168.1.13/new_tms/work/carInfo.json";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        /*
         new Thread(new Runnable() {
             public void run() {
                 //POST
@@ -21,9 +23,13 @@ public class MyActivity extends Activity {
                 params.put("key1","value1");
                 params.put("key2","value2");
                 String result = JieHttpClient.POST(_httpRequestUrl,params);
+                System.out.println("result:"+result);
             }
         }).start();
-
+*/
+        Runnable httpPost = new HttpPostRequest(_httpRequestUrl,"speed:0,battery:50");
+        Thread SendHttpPost = new Thread(httpPost);
+        SendHttpPost.start();
         new Thread(new Runnable() {
             public void run() {
 
