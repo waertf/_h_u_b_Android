55b6aba6f42bf9c10bbd959089a272aced0c00bf
alonso <alonso@wavegis.com.tw>
2014-10-23 17:19:05 +0800

    prevent exception result in crash

--- ODBII/src/com/example/ODBII/HttpPostRequest.java
+++ ODBII/src/com/example/ODBII/HttpPostRequest.java
@@ -19,16 +19,22 @@ public class HttpPostRequest implements Runnable {
     }
     @Override
     public void run() {
-        HashMap<String, String> myMap = new HashMap<String, String>();
-        String s = "SALES:0,SALE_PRODUCTS:1,EXPENSES:2,EXPENSES_ITEMS:3";
-        String[] pairs = parameter.split(",");
-        for (int i=0;i<pairs.length;i++) {
-            String pair = pairs[i];
-            String[] keyValue = pair.split(":");
-            myMap.put(keyValue[0], keyValue[1]);
+        try {
+            HashMap<String, String> myMap = new HashMap<String, String>();
+            String s = "SALES:0,SALE_PRODUCTS:1,EXPENSES:2,EXPENSES_ITEMS:3";
+            String[] pairs = parameter.split(",");
+            for (int i=0;i<pairs.length;i++) {
+                String pair = pairs[i];
+                String[] keyValue = pair.split(":");
+                myMap.put(keyValue[0], keyValue[1]);
+            }
+            result = JieHttpClient.POST(URL,myMap);
+            Log.d(this.toString(),"send http post-" + parameter + "-result:" + result);
         }
-        result = JieHttpClient.POST(URL,myMap);
-        Log.d(this.toString(),"send http post-" + parameter + "-result:" + result);
+        catch (Exception ex){
+            Log.d(this.toString(),ex.toString());
+        }
+
     }
     public String getResult()
     {
